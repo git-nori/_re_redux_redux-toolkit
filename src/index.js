6f567791e01e812810c8ad16154946d9d9177504
HironoriSato <maza.hs5360@gmail.com>
2020-04-09 00:56:10 +0900

    [add]rootReducerの実装, storeの実装とComponentへの組み込み

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 import './index.css';
+
 import App from './App';
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
